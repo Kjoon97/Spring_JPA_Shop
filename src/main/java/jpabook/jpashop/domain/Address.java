1a9aaaa20e3035b4f8a7ad98b18afd635ddbe2b0
joonhyuck <jh83370@naver.com>
2022-02-23 15:34:21 +0900

    엔티티 클래스 개발2

--- src/main/java/jpabook/jpashop/domain/Address.java
+++ src/main/java/jpabook/jpashop/domain/Address.java
@@ -6,10 +6,19 @@ import lombok.Setter;
 import javax.persistence.Embeddable;   //JPA
 
 @Embeddable   //어딘가에 내장될 수 있다
-@Getter @Setter
-public class Address {
+@Getter
+public class Address {  //값 타입은 변경 불가능하게 설계해야 한다. @Setter 를 제거하고, 생성자에서 값을 모두 초기화해서 변경 불가능한 클래스를 만들자
 
     private String city;
     private String street;
     private String zipcode;
+
+    protected Address(){
+    }
+
+    public Address(String city, String street, String zipcode) {
+        this.city = city;
+        this.street = street;
+        this.zipcode = zipcode;
+    }
 }
