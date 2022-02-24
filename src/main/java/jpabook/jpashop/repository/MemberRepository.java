67ab04d0e4e5f9de2ff509ee145ad499313d5a03
joonhyuck <jh83370@naver.com>
2022-02-24 15:22:07 +0900

    회원 리포지토리 개발

---
+++ src/main/java/jpabook/jpashop/repository/MemberRepository.java
@@ -0,0 +1,52 @@
+package jpabook.jpashop.repository;
+
+import jpabook.jpashop.domain.Member;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.util.List;
+
+@Repository
+public class MemberRepository {
+
+    @PersistenceContext
+    private EntityManager em;              //스프링이 EntityManager를 만들어서 주입함.
+
+    /**회원 등록하기 */
+    public void save(Member member){
+        em.persist(member);                 //Jpa가 member를 저장하는 로직.
+    }
+
+
+    /**id로 조회하기 */
+    public Member findOne(Long id){
+        return em.find(Member.class,id);      //JPA가 원하는 member를 찾아서 반환
+    }
+
+
+    /**전부 조회하기 */
+    public List<Member> findAll(){
+        return em.createQuery("select m from Member m", Member.class).getResultList();    //직접 JPQL을 작성해서 JPA에서 회원 목록 가져옴
+    }
+    //sql은 테이블을 대상으로 쿼리를 함.
+    //JPQL은 엔티티 객체를 대상으로 쿼리를 한다. "select m from Member m"은 Member 엔티티의 엘리어스를 m으로 지정하고 엔티티 Member를 조회해라라는 뜻임.
+
+
+    /**이름으로 조회 하기 */
+    public List<Member> findByName(String name){
+        return em.createQuery("select m from Member m where m.name = :name", Member.class)
+                .setParameter("name",name)
+                .getResultList();
+    }
+}
+
+/**
+ * (기본 적으로 @SpringBootApplication이 등록되어있으면 해당 패키지 하위의 모든 @component들을 다 스캔, 스프링 빈으로 자동 등록한다.)
+ * (@Controller, @Repository, @Service도 내부적으로 @component가 들어있어 스캔 대상이 되고 빈에 등록된다.)
+ * 1. @Repository로 해당 리포지토리 클래스가 스프링 빈으로 등록된다.
+ * 2. @PersistContext 를 스프링이 보고 JPA 엔티티 매니저를 해당 객체에 주입해줌.
+ * 3. em.persist(member)하면 영속성 컨텍스트에 member엔티티객체를 넣고 트랜잭션이 커밋 되는 시점에 DB에 반영이된다.( DB에 insert쿼리 날라감)
+ * 4. em.find(타입, 주요 키)  단건 조회.
+ * 5. em.createQuery()로 JPQL 사용.
+ * */
