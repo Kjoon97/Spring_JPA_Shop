e7577c7778f2c02d7709c10f5c0e9af3198525d6
joonhyuck <jh83370@naver.com>
2022-02-25 17:11:55 +0900

    상품 엔티티 개발(비지니스 로직 추가)

--- src/main/java/jpabook/jpashop/domain/item/Item.java
+++ src/main/java/jpabook/jpashop/domain/item/Item.java
@@ -1,6 +1,7 @@
 package jpabook.jpashop.domain.item;
 
 import jpabook.jpashop.domain.Category;
+import jpabook.jpashop.exception.NotEnoughStockException;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -25,4 +26,21 @@ public class Item {
 
     @ManyToMany(mappedBy = "items")
     private List<Category> categories = new ArrayList<>();
+
+    //==비지니스 로직==/
+    /**재고(stock) 증가*/
+    public void addStock(int quantity){
+        this.stockQuantity += quantity;
+    }
+    /**재고(stock) 감소*/
+    public void removeStock(int quantity){
+        int restStock = this.stockQuantity-quantity;
+        if(restStock<0){
+            throw new NotEnoughStockException("need more stock");
+        }
+        this.stockQuantity = restStock;
+    }
+
+    //도메인 주도 설계 엔티티 자체에 해결할 수 있는 건 엔티티 안에 비지니스 로직을 넣는 것이 좋다. 그래야 가장 응집도가 높기 때문이다. 객체 지향적 설계이다.
+    //외부에서 setter로 쓰는 것보다 엔티티에 비지니스 로직을 넣어서 하는 것이 좋다.
 }
