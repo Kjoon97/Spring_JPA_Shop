146166561e55c258b3d0c6ef5d14ab890fff44d2
joonhyuck <jh83370@naver.com>
2022-02-26 15:09:02 +0900

    주문, 주문 상품 엔티티 개발(로직)

--- src/main/java/jpabook/jpashop/domain/Order.java
+++ src/main/java/jpabook/jpashop/domain/Order.java
@@ -51,4 +51,40 @@ public class Order {
         this.delivery = delivery;
         delivery.setOrder(this);
     }
+
+    //==생성 매소드==// 여기서 연관관계, 상태 전부 세팅해줌. 외부에서 복잡하게 Order를 new해서 set을 하는 방식이 아니라 생성할 때부터 무조건 createOrder()가 호출되어 다 세팅해준다.
+    public static Order createOrder(Member member, Delivery delivery, OrderItem... orderItems){
+        Order order = new Order();
+        order.setMember(member);
+        order.setDelivery(delivery);
+        for (OrderItem orderItem : orderItems){
+            order.addOrderItem(orderItem);
+        }
+        order.setStatus(OrderStatus.ORDER);
+        order.setOrderDate(LocalDateTime.now());
+        return order;
+    }
+
+    //==비지니스 로직==//
+    /**주문 취소*/
+    public void cancel(){
+        if(delivery.getStatus() == DeliveryStatus.COMP){
+            throw new IllegalStateException("이미 배송 완료된 상품은 취소가 불가능합니다.");
+        }
+        this.setStatus(OrderStatus.CANCEL);
+        for(OrderItem orderItem: orderItems){
+            orderItem.cancel();
+        }
+    }
+
+    //==조회 로직==//
+    /**전체 주문 가격 조회*/
+    public int getTotalPrice(){
+        int totalPrice=0;
+        for(OrderItem orderItem : orderItems){
+            totalPrice += orderItem.getTotalPrice();
+        }
+        return totalPrice;
+    }
+
 }
