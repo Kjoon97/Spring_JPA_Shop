678f5f8a728beb2628dce22c03c9b8300f293bec
joonhyuck <jh83370@naver.com>
2022-03-08 18:19:17 +0900

    상품 등록

---
+++ src/main/java/jpabook/jpashop/controller/ItemController.java
@@ -0,0 +1,38 @@
+package jpabook.jpashop.controller;
+
+import jpabook.jpashop.domain.item.Book;
+import jpabook.jpashop.service.ItemService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+@RequiredArgsConstructor
+public class ItemController {
+
+    private final ItemService itemService;
+
+    @GetMapping("/items/new")
+    public String createForm(Model model){
+        model.addAttribute("form", new BookForm());
+        return "items/createItemForm";
+    }
+
+    @PostMapping("/items/new")
+    public String create(BookForm form){
+        Book book = new Book();
+        book.setName(form.getName());
+        book.setPrice(form.getPrice());
+        book.setStockQuantity(form.getStockQuantity());
+        book.setAuthor(form.getAuthor());
+        book.setIsbn(form.getIsbn());
+
+        //원래는 book에 생성자를 둬서 파라미터로 받게하는게 set으로 하는 것보다 좋음. 예제이므로 간단히 set으로 함
+
+        itemService.saveItem(book);
+
+        return "redirect:/";
+    }
+}
