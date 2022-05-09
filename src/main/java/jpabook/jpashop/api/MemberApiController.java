0d58248089ccdb87416287da5e0aea76afaab012
joonhyuck <jh83370@naver.com>
2022-05-09 23:03:18 +0900

    회원등록API(실전2)

---
+++ src/main/java/jpabook/jpashop/api/MemberApiController.java
@@ -0,0 +1,51 @@
+package jpabook.jpashop.api;
+
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.service.MemberService;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+import javax.validation.constraints.NotEmpty;
+
+@RestController
+@RequiredArgsConstructor
+public class MemberApiController {
+
+    private final MemberService memberService;
+
+    @PostMapping("/api/v1/members")
+    public CreateMemberResponse saveMemberV1(@RequestBody @Valid Member member){  //@RequestBody 사용하면 json 바디로 받은 것을 member에 매핑해줌.
+        Long id = memberService.join(member);
+        return new CreateMemberResponse(id);
+    }
+
+    @PostMapping("/api/v2/members")
+    public CreateMemberResponse saveMemberV2(@RequestBody @Valid CreateMemberRequest request){ //CreateMemberRequest DTO로 api스펙에 맞게 받기.
+
+        Member member = new Member();
+        member.setName(request.getName());
+        Long id = memberService.join(member);
+        return new CreateMemberResponse(id);
+    }
+
+    @Data
+    static class CreateMemberRequest{  //API 스펙에 맞는 별도의 DTO를 활용함으로써 엔티티를 외부에 노출 시키지 않을 수 있다,- 엔티티와 api스펙을 명확하게 분리할 수 있고 엔티티가
+       // 변경되더라도 api스펙이 변하지 않은 장점이 있다. - 실무에서는 엔티티를 외부에 노출시키거나 파라미터로 받는 건(saveMemberV1같이) 안 하는 것이 좋다.
+
+        @NotEmpty
+        private String name;
+    }
+
+    @Data
+    static class CreateMemberResponse{
+        private Long id;
+
+        public CreateMemberResponse(Long id){
+            this.id = id;
+        }
+    }
+}
