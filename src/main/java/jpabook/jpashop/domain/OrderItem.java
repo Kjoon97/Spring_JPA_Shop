84814ce3bb45dd2c35f6706d5b2ea8d31a20a908
joonhyuck <jh83370@naver.com>
2022-05-15 19:07:56 +0900

    주문 조회 V2: 엔티티를 DTO로 변환

--- src/main/java/jpabook/jpashop/domain/OrderItem.java
+++ src/main/java/jpabook/jpashop/domain/OrderItem.java
@@ -22,7 +22,6 @@ public class OrderItem {
     @Column(name = "order_item_id")
     private Long id;
 
-    @JsonIgnore
     @ManyToOne(fetch = LAZY)
     @JoinColumn(name="item_id")
     private Item item;
